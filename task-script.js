dde95ab0949403b21d91d32f342fd93758547ba0
Larysa1387 <kaliuzhna.off@gmail.com>
2021-04-16 18:05:23 +0300

    task done

--- task-script.js
+++ task-script.js
@@ -15,50 +15,100 @@ console.log(gallery);
 //   для того, чтобы при следующем открытии модального окна, пока грузится
 //   изображение, мы не видели предыдущее.
 
-//  <li class="gallery__item">
-//   <a
-//     class="gallery__link"
-//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//   >
-//     <img
-//       class="gallery__image"
-//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//       alt="Tulips"
-//     />
-//   </a>
-// </li>
-
 const refs = {
-  list: document.querySelector('.js-gallery'),
-
+  galleryList: document.querySelector('.js-gallery'),
+  modalLightBox: document.querySelector('.js-lightbox'),
+  backdrop: document.querySelector('.lightbox__overlay'),
+  currentImgLightBox: document.querySelector('.lightbox__image'),
+  btnCloseModal: document.querySelector('[data-action="close-lightbox"]'),
 }
 
 // Create markup.................
 const listItemsMurkup = createListMurkup(gallery);
-refs.list.innerHTML = listItemsMurkup;
+refs.galleryList.innerHTML = listItemsMurkup;
 function createListMurkup(images) {
-  return images.map(img => `
+  return images.map(({ original, preview, description }) => `
     <li class="gallery__item">
       <a
         class="gallery__link"
-        href="${img.original}"
+        href="${original}"
       >
         <img
           class="gallery__image"
-          src="${img.preview}"
-          data-source="${img.original}"
-          alt="${img.description}"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
         />
       </a>
     </li>`).join('');
 }
-// console.log('~ listItemsMurkup', listItemsMurkup);
 
 // event delegation...................
-refs.list.addEventListener('click', onGetImageUrl);
-function onGetImageUrl(event) {
-  e.preventDefault();
-  const imageUrl = event.target.dataset.source;
-  console.log('~ imageUrl', imageUrl);
+refs.galleryList.addEventListener('click', onGetImage);
+function onGetImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  };
+  refs.currentImgLightBox.src = event.target.dataset.source;
+  refs.currentImgLightBox.alt = event.target.alt;
+  onOpenModal();
 }
+
+// open and close Modal
+function onOpenModal() {
+  refs.modalLightBox.classList.add('is-open');
+  window.addEventListener('keydown', onEscKeyPress);
+  document.addEventListener('keydown', onSliderFunction);
+}
+
+refs.btnCloseModal.addEventListener('click', onCloseModal);
+
+function onCloseModal() {
+  refs.modalLightBox.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.removeEventListener('keydown', onSliderFunction);
+  refs.currentImgLightBox.src = '';
+}
+
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal()
+  }
+}
+
+function onEscKeyPress(event) {
+  const ESC_CODE = 'Escape';
+  if (event.code === ESC_CODE) {
+    onCloseModal();
+  }
+}
+
+// slide to left or right...............
+
+function onSliderFunction(event) {
+  let imagesSrc = [];
+  gallery.map(image => imagesSrc.push(image.original));
+
+  let newIndex = imagesSrc.indexOf(refs.currentImgLightBox.src);
+  const LEFT_ARROW_KEY_CODE = 'ArrowLeft';
+  const RIGHT_ARROW_KEY_CODE = 'ArrowRight';
+
+  if (newIndex < 0) {
+    return;
+  }
+  if (event.code === LEFT_ARROW_KEY_CODE) {
+    newIndex -= 1;
+    if (newIndex === -1) {
+      newIndex = imagesSrc.length - 1;
+    }
+  } else if (event.code === RIGHT_ARROW_KEY_CODE) {
+    newIndex += 1;
+    if (newIndex === imagesSrc.length) {
+      newIndex = 0;
+    }
+  }
+  refs.currentImgLightBox.src = imagesSrc[newIndex];
+};
